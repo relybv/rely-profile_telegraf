d76b7c4157c634d66ad0eb12dada22e06aa1cc92
P. Gomersbach <p.gomersbach@rely.nl>
2017-03-21 21:55:10 +0000

    add plugins

--- manifests/install.pp
+++ manifests/install.pp
@@ -38,5 +38,28 @@ class profile_telegraf::install {
       'ignore_fs' => ['tmpfs', 'devtmpfs'],
     },
   }
-
+  telegraf::input { 'diskio':
+    plugin_type => 'diskio',
+  }
+  telegraf::input { 'kernel':
+    plugin_type => 'kernel',
+  }
+  telegraf::input { 'mem':
+    plugin_type => 'mem',
+  }
+  telegraf::input { 'processes':
+    plugin_type => 'processes',
+  }
+  telegraf::input { 'swap':
+    plugin_type => 'swap',
+  }
+  telegraf::input { 'system':
+    plugin_type => 'system',
+  }
+  telegraf::input { 'net':
+    plugin_type => 'net',
+  }
+  telegraf::input { 'netstat':
+    plugin_type => 'netstat',
+  }
 }
