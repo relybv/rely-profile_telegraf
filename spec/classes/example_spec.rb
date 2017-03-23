34c87e5419d773c806d96ec8535bc6b8ebf98016
P. Gomersbach <p.gomersbach@rely.nl>
2017-03-23 17:07:11 +0000

    add spec tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -20,6 +20,17 @@ describe 'profile_telegraf' do
           it { is_expected.to contain_class('profile_telegraf::service') }
           it { is_expected.to contain_class('telegraf') }
 
+          it { is_expected.to contain_telegraf__input('disk') }
+          it { is_expected.to contain_telegraf__input('diskio') }
+          it { is_expected.to contain_telegraf__input('internal') }
+          it { is_expected.to contain_telegraf__input('kernel') }
+          it { is_expected.to contain_telegraf__input('mem') }
+          it { is_expected.to contain_telegraf__input('net') }
+          it { is_expected.to contain_telegraf__input('netstat') }
+          it { is_expected.to contain_telegraf__input('processes') }
+          it { is_expected.to contain_telegraf__input('swap') }
+          it { is_expected.to contain_telegraf__input('system') }
+
         end
       end
     end
