c23973c56da63b04fd35344b11e89f5a9af68729
P. Gomersbach <p.gomersbach@rely.nl>
2017-03-23 19:01:40 +0000

    add comment

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,7 @@
 #
 class profile_telegraf
 {
+  # commentaar
   class { '::profile_telegraf::install': } ->
   class { '::profile_telegraf::config': } ~>
   class { '::profile_telegraf::service': } ->
