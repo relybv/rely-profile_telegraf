8024570dafbf1e5438de90504211f8944d09b972
P. Gomersbach <p.gomersbach@rely.nl>
2017-04-20 08:40:49 +0000

    add link to class_spec.rb

--- files/spec/acceptance/class_spec.rb
+++ files/spec/acceptance/class_spec.rb
@@ -18,27 +18,18 @@ describe 'profile_telegraf class' do
         EOS
 
         # Run it twice and test for idempotency
-        apply_manifest(pp, :catch_failures => true, :future_parser => true)
-        apply_manifest(pp, :catch_changes  => true, :future_parser => true)
+        apply_manifest(pp, :catch_failures => true)
       end
     end
 
+    describe package('telegraf') do
+      it { is_expected.to be_installed }
+    end
 
-  
-# a profile class should test if the included packages and services are installed, enabled and running. Please adept to your needs. See examples below:
-#   describe package('ntp') do
-#      it { is_expected.to be_installed }
-#    end
-#
-#    describe service('ntp') do
-#      it { is_expected.to be_enabled }
-#      it { is_expected.to be_running }
-#    end
-#
-#    describe port(5432) do
-#      it { should be_listening.with('tcp') }
-#    end
-  
+    describe service('telegraf') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
 
   end
 end
